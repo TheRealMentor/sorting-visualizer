7248120d4a63b763891f6ec16a276a40a8941609
TheRealMentor <sharadr@iitbhilai.ac.in>
2020-03-08 00:45:35 +0530

    Completed Merge Sort

--- src/SortingVisualizer/SortingVisualizer.js
+++ src/SortingVisualizer/SortingVisualizer.js
@@ -3,16 +3,21 @@ import React, { Component } from 'react';
 import './SortingVisualizer.css';
 import * as sortingAlgorithms from '../sortingAlgorithms/sortingAlgorithms';
 
+// Speed const for sorting in milli-second
+const SPEED = 3
+
 class SortingVisualizer extends Component {
 
   state = {
     array: []
   }
 
+  // Reset the array when the app is reloaded
   componentDidMount = () => {
     this.resetArray();
   }
 
+  // Function to reset the array
   resetArray = () => {
     const arr = [];
     for (let i=0; i < 300; i++) {
@@ -22,18 +27,57 @@ class SortingVisualizer extends Component {
     this.setState({ array: arr });
   }
 
+  // Merge Sort function
   mergeSort = () => {
-    const jsSorted = this.state.array.slice().sort((a,b) => a-b);
-    const sortedArr = sortingAlgorithms.mergeSort(this.state.array);
+    const animations = sortingAlgorithms.mergeSort(this.state.array);
 
-    console.log(areArraysEqual(jsSorted, sortedArr));
+    // For every element in triplet in animations, we check the position of that element in that triplet
+    // and then change color if position is 0 or 1 but if position is 2 then we swap the bars.
+    for(let i=0; i < animations.length; i++) {
+      // Getting all bars
+      const arrayBars = document.getElementsByClassName('arr-bar');
+      // Checking if we need to change color or not
+      const isColorChange = i % 3 !== 2;
+      
+      // If yes, then access the DOM and change color
+      if (isColorChange) {
+        
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+
+        // Change color to red, if we are comparing them
+        // Change color to blue, if bars are new in comparison
+        const color = i % 3 === 0 ? "red" : "#3498db";
+
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * SPEED);
+      
+      } else {
+
+        // We overwrite the height of the first bar
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * SPEED);
+      
+      }
+    }
+
   }
 
+  // Function to check the sorting algorithm
   testSortingAlgorithms = () => {
     for(let i=0; i<100; i++) {
+      // Creating a new array everytime
       const array = [];
+      // Providing a new upper bound to length of the array
       const bound = randomIntFromInterval(1, 1000);
       for(let j=0; j < bound; j++) {
+        // Populating the array
         array.push(randomIntFromInterval(-1000, 1000));
       }
       const jsSorted = array.slice().sort((a,b) => a-b);
@@ -68,6 +112,7 @@ function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max-min+1) + min);
 }
 
+// Function to check if arrays are equal or not
 function areArraysEqual(arrOne, arrTwo) {
   if (arrOne.length !== arrTwo.length) return false;
   for (let i=0; i<arrOne.length; i++) {
--- src/sortingAlgorithms/sortingAlgorithms.js
+++ src/sortingAlgorithms/sortingAlgorithms.js
@@ -1,25 +1,113 @@
-export const mergeSort = (array) => {
+// export const mergeSort = (array) => {
 
+//   if (array.length === 1) return array;
+
+//   const middleIdx = Math.floor(array.length / 2);
+//   const left = mergeSort(array.slice(0, middleIdx));
+//   const right = mergeSort(array.slice(middleIdx));
+
+//   const sortedArray = [];
+
+//   let i=0, j=0;
+
+//   while(i<left.length && j<right.length) {
+//     if(left[i] < right[j]) {
+//       sortedArray.push(left[i++]);
+//     } else {
+//       sortedArray.push(right[j++])
+//     }
+//   }
+
+//   while (i<left.length) sortedArray.push(left[i++]);
+//   while (j<right.length) sortedArray.push(right[j++]);
+
+//   return sortedArray;
+// }
+
+export function mergeSort(array) {
+  const animations = [];
   if (array.length === 1) return array;
+  const auxArray = array.slice();
+  mergeSortHelper(array, 0, array.length - 1, auxArray, animations);
+  return animations;
+}
 
-  const middleIdx = Math.floor(array.length / 2);
-  const left = mergeSort(array.slice(0, middleIdx));
-  const right = mergeSort(array.slice(middleIdx));
+// Function that divides the array in 2 parts and apply 
+// the doMerge function to merge the arrays recursively.
+function mergeSortHelper(
+    mainArray, 
+    startIdx, 
+    endIdx, 
+    auxArray, 
+    animations
+  ) {
 
-  const sortedArray = [];
+  if(startIdx === endIdx) return;
+  const middleIdx = Math.floor((startIdx + endIdx)/2);
+  mergeSortHelper(auxArray, startIdx, middleIdx, mainArray, animations);
+  mergeSortHelper(auxArray, middleIdx+1, endIdx, mainArray, animations);
+  doMerge(mainArray, startIdx, middleIdx, endIdx, auxArray, animations);
 
-  let i=0, j=0;
+}
 
-  while(i<left.length && j<right.length) {
-    if(left[i] < right[j]) {
-      sortedArray.push(left[i++]);
+function doMerge(
+    mainArray, 
+    startIdx, 
+    middleIdx, 
+    endIdx, 
+    auxArray, 
+    animations
+  ) {
+  
+  let k = startIdx;
+  let i = startIdx;
+  let j = middleIdx + 1;
+  // Iterating the array and comparing and pushing into animations array
+  while (i<=middleIdx && j<=endIdx) {
+
+    // This push is for coloring the comparing bars
+    animations.push([i,j]);
+    // This push is for coloring the new bars
+    animations.push([i,j]);
+    
+    // Check which bar is smaller
+    if (auxArray[i] <= auxArray[j]) {
+      
+      // Push the height of the smaller bar into animations array
+      animations.push([k, auxArray[i]]);
+      mainArray[k++] = auxArray[i++];
+    
     } else {
-      sortedArray.push(right[j++])
+
+      // Push the height of the smaller bar into animations array
+      animations.push([k, auxArray[j]]);
+      mainArray[k++] = auxArray[j++];
+    
     }
   }
 
-  while (i<left.length) sortedArray.push(left[i++]);
-  while (j<right.length) sortedArray.push(right[j++]);
+  // Checking for the rest of the elements in left array
+  while (i<=middleIdx) {
 
-  return sortedArray;
+    // This push is for coloring the comparing bars
+    animations.push([i, i]);
+    // This push is for coloring the new bars
+    animations.push([i, i]);
+    // Push the height of the smaller bar into animations array
+    animations.push([k, auxArray[i]]);
+
+    mainArray[k++] = auxArray[i++];
+  }
+
+  while (j<=endIdx) {
+    
+    // This push is for coloring the comparing bars
+    animations.push([j, j]);
+    // This push is for coloring the new bars
+    animations.push([j, j]);
+    // Push the height of the smaller bar into animations array
+    animations.push([k, auxArray[j]]);
+
+    mainArray[k++] = auxArray[j++];
+  }
 }
